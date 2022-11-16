caaefdfa013244d34b3a11d3363abbe6b76861d0
Boris Vilka <110785664+NotTheOutsider@users.noreply.github.com>
2022-11-16 10:13:07 +0300

    Update map-script.js

--- js/map-script.js
+++ js/map-script.js
@@ -1,13 +1,30 @@
+let lmao = 0;
+let arrayPlacemarkTest = [];
+
+function getPlacemarks() {
+    $.ajax({
+        url: "dbreceiver.php",
+        type: "POST",
+        data: {"lmao": lmao},
+        success: function (response) {
+            //console.log(response);
+            let result = $.parseJSON(response);
+            arrayPlacemarkTest = Object.values(result);
+            console.log(arrayPlacemarkTest);
+        },
+        error: function (response) {
+        }
+    });
+}
+
+getPlacemarks();
+
 let map;
 let center = [53.904269742126004, 27.566869389465666];
-
-let Placemark1, Placemark2, Placemark3, Placemark4;
 
 let coords = [];
 let GeoObjects = [];
 let Clusterer;
-
-import { arrayPlacemark } from "./placemark-info.js"
 
 let dynamicCoords = [];
 let dynamicPlacemark;
@@ -26,58 +43,45 @@ function init() {
     map.controls.remove('fullscreenControl');
     map.controls.remove('zoomControl');
     map.controls.remove('rulerControl');
+    //     Placemark1 = new ymaps.Placemark([53.90757688355257, 27.55327052323302], {
+    //         balloonContentBody: '<img src="Media/andre-benz-qi2hmCwlhcE-unsplash.jpg" height="150" width="200"> <br/> ',
+    //         hintContent: 'Чорный носок'
+    //     }, {
+    //         preset: 'islands#greenDotIconWithCaption',
+    //         openHintOnHover: true
+    //     });
 
-    function testPlacemarks() {
-        Placemark1 = new ymaps.Placemark([53.90757688355257, 27.55327052323302], {
-            balloonContentBody: '<img src="Media/andre-benz-qi2hmCwlhcE-unsplash.jpg" height="150" width="200"> <br/> ',
-            hintContent: 'Чорный носок'
-        }, {
-            preset: 'islands#greenDotIconWithCaption',
-            openHintOnHover: true
-        });
+    //     Placemark2 = new ymaps.Placemark([53.907380497270964, 27.553420726937862], {
+    //         hintContent: 'Бели носок',
+    //         balloonContent: 'bruh'
+    //     }, {
+    //         preset: 'islands#greenDotIconWithCaption',
+    //         openHintOnHover: true
+    //     });
 
-        Placemark2 = new ymaps.Placemark([53.907380497270964, 27.553420726937862], {
-            hintContent: 'Бели носок',
-            balloonContent: 'bruh'
-        }, {
-            preset: 'islands#greenDotIconWithCaption',
-            openHintOnHover: true
-        });
+    //     Placemark3 = new ymaps.Placemark([53.90771071555245, 27.55464721317863], {
+    //         hintContent: 'Желтый носок'
+    //     }, {
+    //         preset: 'islands#greenDotIconWithCaption',
+    //         openHintOnHover: true
+    //     });
 
-        Placemark3 = new ymaps.Placemark([53.90771071555245, 27.55464721317863], {
-            hintContent: 'Желтый носок'
-        }, {
-            preset: 'islands#greenDotIconWithCaption',
-            openHintOnHover: true
-        });
+    //     Placemark4 = new ymaps.Placemark([53.90765167611442, 27.55164783377525], {
+    //         hintContent: 'Красный носок'
+    //     }, {
+    //         preset: 'islands#greenDotIconWithCaption',
+    //         openHintOnHover: true
+    //     });
 
-        Placemark4 = new ymaps.Placemark([53.90765167611442, 27.55164783377525], {
-            hintContent: 'Красный носок'
-        }, {
-            preset: 'islands#greenDotIconWithCaption',
-            openHintOnHover: true
-        });
-
-    };
+    // };
 
     function cluster() {
-        coords = [
-            Placemark1,
-            Placemark2,
-            Placemark3,
-            Placemark4
-        ];
-
         GeoObjects = [];
-
-        for (var i = 0; i < coords.length; i++) {
-            GeoObjects[i] = coords[i];
-        }
 
         Clusterer = new ymaps.Clusterer({
             preset: 'islands#invertedGreenClusterIcons',
             groupByCoordinates: false,
-            gridSize: 256,
+            gridSize: 128,
         });
 
         Clusterer.add(GeoObjects);
@@ -86,21 +90,23 @@ function init() {
 
     function dbPlacemarks() {
 
-        for (let i = 0; i <= 2; i++) {
-            let lat = arrayPlacemark[i].coordsLat;
-            let lon = arrayPlacemark[i].coordsLon;
+        for (let i = 0; i < (arrayPlacemarkTest.length); i++) {
+            console.log(arrayPlacemarkTest[i][1], arrayPlacemarkTest[i][2],arrayPlacemarkTest[i][3],arrayPlacemarkTest[i][4],arrayPlacemarkTest[i][5])
+            let lat = arrayPlacemarkTest[i][2];
+            let lon = arrayPlacemarkTest[i][3];
 
-            arrayPlacemark[i] = new ymaps.Placemark([lat, lon], {
-                balloonContentHeader: arrayPlacemark[i].balloonText,
-                balloonContentBody: `<img src=\"${arrayPlacemark[i].ballonImgUrl}\" height=\"150\" width=\"200\"> <br/>`,
-                hintContent: arrayPlacemark[i].hint
+            arrayPlacemarkTest[i][1] = new ymaps.Placemark([lat, lon], {
+                balloonContentHeader: arrayPlacemarkTest[i][4],
+                balloonContentBody: `${arrayPlacemarkTest[i][5]}\n
+                <img src=\"${arrayPlacemarkTest[i][6]}\" height=\"150\" width=\"200\"> <br/>`,
+                hintContent: arrayPlacemarkTest[i][4]
             }, {
                 preset: 'islands#greenDotIconWithCaption',
                 balloonMaxWidth: 200,
                 openHintOnHover: true
             });
 
-            GeoObjects.push(arrayPlacemark[i]);
+            GeoObjects.push(arrayPlacemarkTest[i][1]);
             Clusterer.add(GeoObjects);
             map.geoObjects.add(Clusterer);
         }
@@ -112,12 +118,9 @@ function init() {
 
         let oneCall = 1;
 
-		//   
-		// моя добавка ниже 
-		//	
-
         map.events.add('click', function (item) {
             dynamicCoords = item.get('coords');
+            coordsToSend();
             if (oneCall == 1) {
                 dynamicPlacemark = new ymaps.Placemark([dynamicCoords[0], dynamicCoords[1]], {
                 }, {
@@ -128,9 +131,6 @@ function init() {
 
                 deleteButton.classList.add('shown');
 
-					 console.log('delete button is shown')
-					 form.classList.add('_active')
-
                 map.geoObjects.add(dynamicPlacemark);
 
                 oneCall++;
@@ -140,45 +140,84 @@ function init() {
             }
         });
 
-        cancelButton.onclick = function () {
+        deleteButton.onclick = function () {
             deleteButton.classList.remove('shown');
-				console.log("delete button isn't shown")
-				console.log()
             map.geoObjects.remove(dynamicPlacemark);
             oneCall--;
         }
 
     };
 
-    testPlacemarks();
     cluster();
     dbPlacemarks();
     getCoords();
 };
 
-// $(document).ready(function () {
-//     $("#btn").click(
-//         function () {
-//             sendAjaxForm('result_form', 'ajax_form', 'dbquery.php');
-//             return false;
-//         }
-//     );
-// });
+let coordsLat;
+let coordsLon;
+function coordsToSend() {
+    coordsLat = dynamicCoords[0];
+    coordsLon = dynamicCoords[1];
+    console.log(coordsLat, coordsLon);
+}
 
-// function sendAjaxForm(result_form, ajax_form, url) {
-//     $.ajax({
-//         url: url,
-//         type: "POST",
-//         dataType: "html", 
-//         data: $("#" + ajax_form).serialize(),
-//         success: function (response) {
-//             let result = $.parseJSON(response);
-//             $('#result_form').html('How: ' + result.name + '<br>to work: ' + result.notname);
-//         },
-//         error: function (response) {
-//             $('#result_form').html('Ошибка. Данные не отправлены.');
-//         }
-//     });
-// }
+let imgForm = document.getElementById('balloonImgHTML');
+let reader = [];
+let imgUrl;
+
+imgForm.addEventListener('change', imgToUrl);
+function imgToUrl() {
+    reader[0] = new FileReader();
+    reader[0].readAsDataURL(this.files[0]);
+    (() => {
+        reader[0].onload = (e) => {
+            imgUrl = e.target.result;
+            //console.log(imgUrl);
+        }
+    })()
+}
+
+let hintHTML;
+let balloonTextHTML;
+
+$(document).ready(function () {
+    $("#button").click(
+        function () {
+            hintHTML = document.getElementById('hintHTML').value;
+            balloonTextHTML = document.getElementById('balloonTextHTML').value;
+            sendAjaxForm('dbsender.php');
+            return false;
+        }
+    );
+});
+
+function sendAjaxForm(url) {
+    $.ajax({
+        url: url,
+        type: "POST",
+        dataType: "html",
+        data: {
+            "hintHTML": hintHTML,
+            "balloonTextHTML": balloonTextHTML,
+            "balloonImgHTML": imgUrl,
+            "coordsLat": coordsLat,
+            "coordsLon": coordsLon,
+        },
+        success: function (response) {
+            /*console.log(response);
+            let result = $.parseJSON(response);
+            console.log(
+                result.hintHTML, 
+                result.balloonTextHTML, 
+                //result.balloonImgHTML, 
+                result.coordsLat, 
+                result.coordsLon
+                )*/
+        },
+        error: function (response) {
+            $('#form').html('Ошибка. Данные не отправлены.');
+        }
+    });
+}
 
 ymaps.ready(init);
